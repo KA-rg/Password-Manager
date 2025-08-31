58b58538e9658f20526c8f1e6dc7c5fc17921f9a
KA-rg <krishjain260905@gmail.com>
2025-09-01 02:16:46 +0530

    express/wrap error made

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-document.querySelector(".btn").addEventListener("click", (event) => {
-  event.preventDefault();
-  let passwords = localStorage.getItem("passwords");
-  if (passwords == null){
-    let json = [];
-    json.push({username: username.value, password: password.value});
-    alert("Password Saved");
-    localStorage.setItem("passwords", JSON.stringify(json))
-  }
-  else{
-    let json = JSON.parse(localStorage.getItem("passwords"));
-    json.push({username: username.value, password: password.value});
-    alert("Password Saved");
-    localStorage.setItem("passwords", JSON.stringify(json));
-  }
-});
+// document.querySelector(".btn").addEventListener("click", (event) => {
+//   event.preventDefault();
+//   let passwords = localStorage.getItem("passwords");
+//   if (passwords == null){
+//     let json = [];
+//     json.push({username: username.value, password: password.value});
+//     alert("Password Saved");
+//     localStorage.setItem("passwords", JSON.stringify(json))
+//   }
+//   else{
+//     let json = JSON.parse(localStorage.getItem("passwords"));
+//     json.push({username: username.value, password: password.value});
+//     alert("Password Saved");
+//     localStorage.setItem("passwords", JSON.stringify(json));
+//   }
+// });
+
